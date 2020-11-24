a7d51ca16d5e776821006b2b5182c781da2d441b
Forever-Incognito <andrii2003mak@gmail.com>
2020-11-24 18:03:48 +0200

    1

---
+++ UnitTest 6.2_iter/UnitTest 6.2_iter.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62iter
+{
+	TEST_CLASS(UnitTest62iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 18;
+			int a[n];
+		}
+	};
+}
